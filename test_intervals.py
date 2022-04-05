42c7444238828722616fcfbde4f2088f004fc1c2
ayushi0916 <86554954+ayushi0916@users.noreply.github.com>
2022-04-04 23:33:46 -0700

    Update test_intervals.py

--- test_intervals.py
+++ test_intervals.py
@@ -1,4 +1,4 @@
-"""Test suite to accompany intervals.py.
+"""Test suite to accompany fractions.py.
 
 All of our CIS 211 projects will include unit test suites.  Some of them will be
 provided to you.  Others you will write yourself, or you will be required to add
@@ -6,73 +6,63 @@ to an existing unit test suite.
 """
 
 import unittest
-import intervals
+import fractions
 
-class Test_01_Intervals(unittest.TestCase):
-    """Initial tests of the Interval class"""
+class Test_01_Fraction(unittest.TestCase):
+    """Initial tests of the Fraction class"""
 
     def test_00_can_create(self):
-        """Can I create an interval from 3..5?"""
+        """Can I create an fraction of 6/8?"""
         try:
-            i = intervals.Interval(3,5)
+            i = fractions.Fraction(6, 8)
         except:
-            self.fail("Unable to create Interval(3,5)")
+            self.fail("Unable to create Fraction(6,8)")
 
     def test_01_oriented(self):
-        """The low end of interval must not exceed the high end (test in constructor)"""
+        """Neither the numerator nor the denominator can be < 0 and the denominator cannot be <= 0"""
         with self.assertRaises(expected_exception=Exception):
-            i = intervals.Interval(5,3)
+            i = fractions.Fraction(-3, 4)
+            i = fractions.Fraction(4, 0)
 
-    def test_02_membership(self):
-        """Interval(m,n) contains exactly p such that m <= p <= n"""
-        i = intervals.Interval(-1,1)
-        self.assertFalse(i.contains(-12))
-        self.assertFalse(i.contains(-2))
-        self.assertTrue(i.contains(-1))
-        self.assertTrue(i.contains(0))
-        self.assertTrue(i.contains(1))
-        self.assertFalse(i.contains(2))
-        self.assertFalse(i.contains(15))
+    def test_02_str(self):
+        """Magic method is used whenever you print an object"""
+        a = fractions.Fraction(5, 8)
+        self.assertEqual(a.__str__(), '[5/8]')
+        b = fractions.Fraction(1, 2)
+        self.assertEqual(b.__str__(), '[1/2]')
 
-    def test_03_overlaps(self):
-        """Intervals overlap if they contain any of the same elements"""
-        i = intervals.Interval(1,3)
-        self.assertTrue(i.overlaps(intervals.Interval(2,5)))
-        self.assertFalse(i.overlaps(intervals.Interval(4,5)))
-        self.assertFalse(i.overlaps(intervals.Interval(-3,0)))
-        self.assertFalse(i.overlaps(intervals.Interval(12,17)))
+    def test_03_repr(self):
+        """A string that looks like a call to the constructor of the class"""
+        i = fractions.Fraction(3, 8)
+        self.assertEqual(i.__repr__(), 'Fraction(3, 8)')
 
-    def test_04_equality(self):
-        """Intervals are equal if they have the same low and high bound"""
-        x = intervals.Interval(3,5)
-        y = intervals.Interval(3,5)
-        z = intervals.Interval(3,7)
-        w = intervals.Interval(4,5)
-        self.assertTrue(x == y)
-        self.assertFalse(x == z)
-        self.assertFalse(x == w)
+    def test_04_mul(self):
+        """multiply both the fractions"""
+        a = fractions.Fraction(3, 4)
+        self.assertTrue(__eq__(a.__mul__(fractions.Fraction(2, 5)), fractions.Fraction(6, 20)))
 
-    def test_05_bad_join(self):
-        """Can't join intervals unless they overlap"""
-        i = intervals.Interval(0, 3)
-        j = intervals.Interval(4, 8)
-        with self.assertRaises(Exception):
-            k = i.join(j)
+    def test_05_add(self):
+        """Adding both the fractions"""
+        a = fractions.Fraction(3, 4)
+        self.assertTrue(__eq__(a.__add__(fractions.Fraction(2, 5)), fractions.Fraction(23, 20)))
 
-    def test_06_good_joins(self):
-        """Join of overlapping intervals covers their union"""
-        a = intervals.Interval(0,4)
-        b = intervals.Interval(2,3)
-        c = intervals.Interval(3,5)
-        self.assertEqual(a.join(b), a)
-        self.assertEqual(b.join(a), a)
-        self.assertEqual(a.join(c), intervals.Interval(0,5))
-        self.assertEqual(b.join(c), intervals.Interval(2,5))
+    def test_06_simplify(self):
+        """simplifying the fractions"""
+        a = fractions.Fraction(35, 56)
+        self.assertTrue(__eq__(a.simplify(), fractions.Fraction(5, 8)))
 
+
+def __eq__(a: "Fraction", b: "Fraction") -> "Fraction":
+    """Intervals are equal if they have the same low and high bounds"""
+    return a.num == b.num and a.den == b.den
 
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
+
+
+
+
 
 
 
